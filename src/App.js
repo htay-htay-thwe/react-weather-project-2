6d5ed2bd0bd00cbaa9ca5051f85a559904a3ee6c
htay-htay-thwe <htayhtaythwe962@gmail.com>
2023-05-12 17:28:13 +0630

    bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 
 import './App.css';
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
   return (
     <div className="App">
-      Weather App
-       
-        
+      <h1> Weather App</h1>
+      <p>
+        <a href="https://github.com/htay-htay-thwe/react-weather-project-2">
+          open sourced-code
+        </a>{" "}
+        by Htay Htay Thwe
+      </p>
     </div>
   );
 }
